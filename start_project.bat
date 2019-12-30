4be091c4c87ae7622373934a7bf93102eec4776d
AndreiWess <48791679+AndreiWess@users.noreply.github.com>
2019-12-30 20:23:57 +0200

    Update start_project.bat

--- start_project.bat
+++ start_project.bat
@@ -1,1 +1,2 @@
-C:\venvs\faculty\ia_venv\Scripts\python.exe server.py
+::C:\venvs\faculty\ia_venv\Scripts\python.exe server.py
+C:\Users\Wessley\AppData\Local\Microsoft\WindowsApps\python.exe server.py
